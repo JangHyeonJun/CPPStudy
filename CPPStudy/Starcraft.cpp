c581a91bfefcd6bec11d35ae60d33daa54d1ad2b
JangMotti <totok682@naver.com>
2019-07-12 19:04:23 +0900

    ~ c++ 4-4 section

--- CPPStudy/Starcraft.cpp
+++ CPPStudy/Starcraft.cpp
@@ -2,15 +2,16 @@
 //
 //class Marine
 //{
+//	static int total_marine_num;
 //	int hp;
 //	int coord_x, coord_y;
-//	int damage;
+//	const int damage;
 //	bool is_dead;
 //	char* name;
 //
 //public:
 //	Marine();
-//	Marine(int x, int y, const char* marine_name);
+//	Marine(int x, int y, const char* marine_name, const int defualt_damage);
 //	~Marine();
 //
 //	int attack();
@@ -18,25 +19,15 @@
 //	void move(int x, int y);
 //
 //	void show_status();
+//	static void show_total_marine();
 //};
-//Marine::Marine()
-//{
-//	hp = 50;
-//	coord_x = coord_y = 0;
-//	damage = 5;
-//	is_dead = false;
-//	name = NULL;
-//}
-//Marine::Marine(int x, int y, const char* marine_name)
+//Marine::Marine() : hp(50), coord_x(0), coord_y(0), damage(5), is_dead(false), name(NULL) { total_marine_num++; }
+//Marine::Marine(int x, int y, const char* marine_name, const int defualt_damage = 5) 
+//	: hp(50), coord_x(x), coord_y(y), damage(defualt_damage), is_dead(false), name(name)
 //{
 //	name = new char[strlen(marine_name) + 1];
 //	strcpy_s(name, strlen(marine_name)+1, marine_name);
-//
-//	coord_x = x;
-//	coord_y = y;
-//	hp = 50;
-//	damage = 5;
-//	is_dead = false;
+//	total_marine_num++;
 //}
 //void Marine::move(int x, int y)
 //{
@@ -62,7 +53,10 @@
 //	std::cout << name << " 의 소멸자 호출 ! " << std::endl;
 //	if (name != NULL)
 //		delete[] name;
+//	total_marine_num--;
 //}
+//void Marine::show_total_marine() { std::cout << "전체 마린 수 : " << total_marine_num << std::endl; }
+//int Marine::total_marine_num = 0;
 //
 //int main()
 //{
@@ -85,6 +79,7 @@
 //
 //	marines[0]->show_status();
 //	marines[1]->show_status();
+//	Marine::show_total_marine();
 //
 //	std::cout << std::endl << "마린 1 이 마린 2 를 공격! " << std::endl;
 //	
@@ -92,9 +87,11 @@
 //
 //	marines[0]->show_status();
 //	marines[1]->show_status();
+//
 //	
 //	delete marines[0];
 //	delete marines[1];
+//	Marine::show_total_marine();
 //
 //	return 0;
 //}
