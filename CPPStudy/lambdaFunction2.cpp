979d762113e0802f8b8561fb14079c1eb47daa56
JangMotti <totok682@naver.com>
2019-09-19 15:23:19 +0900

    studying lambda function and range based loop

---
+++ CPPStudy/lambdaFunction2.cpp
@@ -0,0 +1,24 @@
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//using namespace std;
+//
+//int main()
+//{
+//	vector<int> v = { 10,9,20,1,4,5,8,7 };
+//
+//	for (auto i : v)
+//		cout << i << ' ';
+//	cout << endl;
+//
+//	sort(v.begin(), v.end());
+//	for (auto i : v)
+//		cout << i << ' ';
+//	cout << endl;
+//
+//	sort(v.begin(), v.end(), [](int a, int b)->bool{ return a > b; });
+//	[](vector<int> v)-> void {for (auto i : v) cout << i << ' '; }(v);
+//	cout << endl;
+//
+//	return 0;
+//}
